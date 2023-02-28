f67880d70458eb4ef7e3418df2de2ab3dd85d988
cadenforrest <cadenforrest@gmail.com>
2023-02-28 02:38:41 -0600

    break out some stuff into a module or whatever rust calls it, add tests

---
+++ src/lib.rs
@@ -0,0 +1,24 @@
+use anyhow::Result;
+use colored::Colorize;
+use std::fs;
+use std::path::Path;
+
+/// Matches content in a file, writes results to writer
+pub fn grep_file(path: &Path, pattern: &String, mut writer: impl std::io::Write) -> Result<()> {
+    let content = match fs::read_to_string(path) {
+        Ok(content) => content,
+        Err(_) => return Ok(()), // skip if it's not UTF-8
+    };
+    for (line_number, line) in content.lines().enumerate() {
+        if line.to_lowercase().contains(&pattern.to_lowercase()) {
+            writeln!(writer, "{}", path.display().to_string().green())?;
+            writeln!(
+                writer,
+                "{}:{}",
+                (line_number + 1).to_string().cyan(),
+                line.trim().white(),
+            )?;
+        }
+    }
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,13 @@
 use anyhow::Result;
 use clap::Parser;
-use colored::Colorize;
-use std::fs;
-use std::path::{Path, PathBuf};
+use std::path::PathBuf;
+use superdupergrep::grep_file;
 use walkdir::WalkDir;
 
 #[derive(Parser)]
 struct Cli {
     /// The pattern to look for
-    pattern: PathBuf,
+    pattern: String,
     /// The path to the file or directory to search
     path: PathBuf,
 }
@@ -20,34 +19,30 @@ fn main() -> Result<()> {
         for entry in WalkDir::new(&args.path) {
             let entry = entry?;
             if entry.file_type().is_file() {
-                grep_file(&entry.path(), &args.pattern)?;
+                grep_file(&entry.path(), &args.pattern, &mut std::io::stdout())?;
             }
         }
     } else {
-        // search the single file
-        grep_file(&args.path, &args.pattern)?;
+        grep_file(&args.path, &args.pattern, &mut std::io::stdout())?;
     }
     Ok(())
 }
 
-// Matches content in a file, prints to std::out
-fn grep_file(path: &Path, pattern: &Path) -> Result<()> {
-    let content = match fs::read_to_string(path) {
-        Ok(content) => content,
-        Err(_) => return Ok(()), // skip if it's not UTF-8
-    };
-    for (line_number, line) in content.lines().enumerate() {
-        if line
-            .to_lowercase()
-            .contains(&pattern.to_string_lossy().to_lowercase())
-        {
-            println!("{}", path.display().to_string().green());
-            println!(
-                "{}:{}",
-                (line_number + 1).to_string().cyan(),
-                line.trim().white(),
-            );
-        }
+#[cfg(test)]
+mod tests {
+    use assert_cmd::Command;
+    use assert_fs::prelude::FileWriteStr;
+
+    #[test]
+    pub fn find_content_in_file() -> Result<(), Box<dyn std::error::Error>> {
+        let file = assert_fs::NamedTempFile::new("sample.txt")?;
+        file.write_str("Hi!\nHello!\nWhat's up!\n")?;
+
+        let mut cmd = Command::cargo_bin("superdupergrep")?;
+        cmd.arg("hello").arg(file.path());
+        cmd.assert()
+            .success()
+            .stdout(predicates::str::contains("Hello!\n"));
+        Ok(())
     }
-    Ok(())
 }
